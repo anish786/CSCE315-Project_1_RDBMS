cc56e0bb12b9862938b7f908c67f56222b579ec5
Nathan Kocmoud <nathan.kocmoud@gmail.com>
2014-02-04 14:41:12 -0600

    Adding comment about what needs to be implemented

--- Car Dealership/RDBMS/Database.cpp
+++ Car Dealership/RDBMS/Database.cpp
@@ -1,5 +1,26 @@
 #include "Database.h"
 
+/*
+Imlementations needed:
+
+CREATE TABLE (PRIMARY KEY)
+DROP TABLE
+
+INSERT INTO
+UPDATE
+DELETE FROM
+
+SELECTION
+PROJECTION
+RENAMING
+SET UNION
+SET DIFFERENCE
+CROSS PRODUCT
+
+No file I/0
+No parser (everything is through funtion call)
+*/
+
 
 Database::Database(){
 
