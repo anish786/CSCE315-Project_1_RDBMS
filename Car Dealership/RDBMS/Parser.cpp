263658f844b5e65d4c72f5941d8a6c360bf3d620
Michael Stephens <md.stephens@neo.tamu.edu>
2014-02-07 16:31:04 -0600

    Minimal Work on Parser

--- Car Dealership/RDBMS/Parser.cpp
+++ Car Dealership/RDBMS/Parser.cpp
@@ -1,11 +1,23 @@
+#include <cstdlib>
 #include "Parser.h"
 
+using namespace std;
 
 Parser::Parser(){
-	//comment
 }
 
-
 Parser::~Parser(){
+}
 
+void Parser::parse(const string& input) {
+	if (input.substr(0, 10) == "OPEN ") {
+		sscanf_s(input.c_str() + 10, "%s", playerId);
+		int q1 = input.find('"');
+		int q2 = input.rfind('"');
+		if (q1 != string::npos
+			&& q2 != string::npos
+			&& q2 - q1 > 2) {
+			*playerName = input.substr(q1 + 1, q2 - q1 - 1);
+		}
+	}
 }
--- Car Dealership/RDBMS/Parser.h
+++ Car Dealership/RDBMS/Parser.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <string>
+#include <vector>
+
 class Parser
 {
 public:
 	Parser();
+	void parse(const std::string& input);
 	~Parser();
 };
 
