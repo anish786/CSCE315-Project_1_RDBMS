513fbfaf0ce1f2c7a06a2edcbb2c186077c4a429
Nathan <nathan.kocmoud@gmail.com>
2014-02-07 16:26:02 -0600

    specified comments

--- Car Dealership/RDBMS/Database.cpp
+++ Car Dealership/RDBMS/Database.cpp
@@ -4,18 +4,20 @@
 Imlementations needed:
 
 CREATE TABLE (PRIMARY KEY)
-DROP TABLE
+DROP TABLE			Delete a table
 
-INSERT INTO
-UPDATE
-DELETE FROM
+INSERT INTO			Insert Tuble into a relation
+UPDATE				Update a Tuple
+DELETE FROM			Delete a Tuble from a relation
 
-SELECTION
-PROJECTION
-RENAMING
-SET UNION
-SET DIFFERENCE
-CROSS PRODUCT
+SELECTION			Creates a subset of the tuples in a relation that satisfy a condition
+PROJECTION			Creates a relation that lists only a subset of columns (attributes) for the tuples
+RENAMING			
+SET UNION			Set of all elements in A or B or both
+SET DIFFERENCE		Set of all elements that are either in A or B
+CROSS PRODUCT		Creates a relation between two relations of all the sets of pairs between the two
+(NATURAL JOIN)		Combine two relations that have at least one matching attribute, delete tuples that do not share matching
+					values in the matching relation.
 
 No file I/0
 No parser (everything is through funtion call)
