bf5172a39b70ab4a4e6f3f65f6967eac03271453
crapier <c.rapier@hotmail.com>
2014-02-10 18:44:12 -0600

    fixed cell comparison

--- Car Dealership/RDBMS/Cell.h
+++ Car Dealership/RDBMS/Cell.h
@@ -33,9 +33,15 @@ public:
 		if (cell_1.cell_type == 0 && cell_2.cell_type == 0){
 			return (cell_1.cell_data.num == cell_2.cell_data.num);
 		}
-		else if (cell_1.cell_type == 1 && cell_2.cell_type == 1){
-			return (*cell_1.cell_data.word == *cell_2.cell_data.word);
+		else if (cell_1.cell_type == 1 && cell_2.cell_type == 1 && cell_1.word_length == cell_2.word_length){
+			for(int i = 0; i < cell_1.word_length; i++){
+				if(cell_1.cell_data.word[i] != cell_2.cell_data.word[i]){
+					return false;
+				}
+			}
+			return true;
 		}
+		return false;
 	}
 	Cell& operator=(const Cell c);
 
