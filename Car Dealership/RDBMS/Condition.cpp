ddc900d57636169a38b0b308c0a701980f5682e6
Clifton Rapier <c.rapier@hotmail.com>
2014-02-13 18:14:12 -0600

    Conditions can now be recursive

--- Car Dealership/RDBMS/Condition.cpp
+++ Car Dealership/RDBMS/Condition.cpp
@@ -1,17 +1,95 @@
 #include "Condition.h"
 
+int string_to_int(string s){
+	stringstream ss(s);
+	int i;
+	ss >> i;
+	return i;
+}
+
+Condition::Condition(){
+
+}
 
 Condition::Condition(vector<Conjunction> con){
 	conjunctions = con;
 }
 
-Condition::~Condition(void){
-
-}
-
 bool Condition::evaluate(vector<string> variables, vector<string> values){
 	for(size_t i = 0; i < conjunctions.size(); i++){
 		if(conjunctions[i].evaluate(variables, values)){
+			return true;
+		}
+	}
+	return false;
+}
+
+Conjunction::Conjunction(vector<Comparison> comp){
+	comparisons = comp;
+}
+
+bool Conjunction::evaluate(vector<string> variables, vector<string> values){
+	for (size_t i = 0; i < comparisons.size(); i++){
+		if (!comparisons[i].evaluate(variables, values)){
+			return false;
+		}
+	}
+	return true;
+}
+
+Comparison::Comparison(Condition cond){
+	condition = cond;
+	is_condition = true;
+}
+
+Comparison::Comparison(string o1, string o2, string o){
+	operand1 = o1;
+	operand2 = o2;
+	op = o;
+	is_condition = false;
+}
+
+bool Comparison::evaluate(vector<string> variables, vector<string> values){
+	if (is_condition){
+		return condition.evaluate(variables, values);
+	}
+	string eval_operand1 = operand1;
+	string eval_operand2 = operand2;
+	for (size_t i = 0; i < variables.size(); i++){
+		if (operand1.compare(variables[i]) == 0){
+			eval_operand1 = values[i];
+		}
+		if (operand2.compare(variables[i]) == 0){
+			eval_operand2 = values[i];
+		}
+	}
+	if (op.compare("==") == 0){
+		if (eval_operand1.compare(eval_operand2) == 0){
+			return true;
+		}
+	}
+	else if (op.compare("!=") == 0){
+		if (eval_operand1.compare(eval_operand2) != 0){
+			return true;
+		}
+	}
+	else if (op.compare("<") == 0){
+		if (string_to_int(eval_operand1) < string_to_int(eval_operand2)){
+			return true;
+		}
+	}
+	else if (op.compare(">") == 0){
+		if (string_to_int(eval_operand1) > string_to_int(eval_operand2)){
+			return true;
+		}
+	}
+	else if (op.compare("<=") == 0){
+		if (string_to_int(eval_operand1) <= string_to_int(eval_operand2)){
+			return true;
+		}
+	}
+	else if (op.compare(">=") == 0){
+		if (string_to_int(eval_operand1) >= string_to_int(eval_operand2)){
 			return true;
 		}
 	}
--- Car Dealership/RDBMS/Condition.h
+++ Car Dealership/RDBMS/Condition.h
@@ -2,16 +2,44 @@
 
 #include <vector>
 #include <string>
-#include "Conjunction.h"
+#include <sstream>
 
 using namespace std;
+
+// Helper function
+int string_to_int(string s);
+
+class Conjunction;
+class Comparison;
 
 class Condition
 {
 	vector<Conjunction> conjunctions;
 public:
+	Condition();
 	Condition(vector<Conjunction> con);
-	~Condition(void);
+	bool evaluate(vector<string> variables, vector<string> values);
+};
+
+class Conjunction
+{
+	vector<Comparison> comparisons;
+public:
+	Conjunction(vector<Comparison> comp);
+	bool evaluate(vector<string> variables, vector<string> values);
+};
+
+class Comparison
+{
+	string operand1;
+	string operand2;
+	string op;
+	bool is_condition;
+	Condition condition;
+
+public:
+	Comparison(Condition cond);
+	Comparison(string o1, string o2, string o);
 	bool evaluate(vector<string> variables, vector<string> values);
 };
 
