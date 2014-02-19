1a5e42c08cfbb589d0816b955de3f6dd7e4189f4
Anish <ali.anish91@gmail.com>
2014-02-19 14:53:21 -0600

    added show_customer, add_customer functions and little bit changes in main function.

--- Car Dealership/Car Dealership/main.cpp
+++ Car Dealership/Car Dealership/main.cpp
@@ -2,9 +2,6 @@
 
 // prints main menu
 void main_menu(){
-	cout << "\n\n********************************************************************************"
-		 << "\n\t\t WELCOME TO CARS2GO AUTO DEALERSHIP!\n"
-		 << "\n********************************************************************************";
 	cout << "\t\t\t*********************"<<endl;
 	cout << "\t\t\t*     Main Menu     *"<<endl;
 	cout << "\t\t\t*********************"<<endl;
@@ -57,9 +54,41 @@ void print_salesperson(){
 	cout <<"\t\t\t******************************"<<endl;
 	cout <<"Choose option: ";
 }
+void show_customer(){
+	string input = "SHOW customers";
+	Parser p;
+	p.parse_command(input);
+}
+void add_customer(){
+	string first_name;
+	cout << "First name: ";
+	cin >> first_name;
+	string last_name;
+	cout << "Last name: ";
+	cin >> last_name;
+	char gender;
+	cout << "Gender: ";
+	cin >> gender;
+	string customer_id;
+	cout << "Customer ID: ";
+	cin >> customer_id;
+	int age;
+	cout << "Age: ";
+	cin >> age;
+
+	string input = "INSERT INTO customers VALUES FROM (" + first_name + ", " + last_name + ", " + customer_id + ", " + gender + ", " + (char)age + ")";
+	Parser p;
+	p.parse_command(input);
+
+	cout << "Customer inserted into database" << endl;
+}
 // main method
 int main(){
+	cout << "\n\n********************************************************************************"
+	 << "\n\t\t WELCOME TO CARS2GO AUTO DEALERSHIP!\n"
+	 << "\n********************************************************************************";
 	int choice = 0;
+	int select = 0;
 	string command;
 	//----------------
 	main_menu();
@@ -69,10 +98,34 @@ int main(){
 			print_cars();
 		}
 		else if(choice == 2){
-			print_customers();
+			print_salesperson();
 		}
 		else if(choice == 3){
-			print_salesperson();
+			print_customers();
+			cin >> select;
+			if(select == 1){
+				show_customer();
+				cout <<"Choose option: ";
+				cin >> select;
+			}
+			if(select == 2){
+
+			}
+			if(select == 3){
+				add_customer();
+				print_customers();
+				cin >> select;
+			}
+			if(select == 4){
+
+			}
+			if(select == 5){
+
+			}
+			if(select == 6){
+				main_menu();
+
+			}
 		}
 		else if(choice == 4){
 			break;
